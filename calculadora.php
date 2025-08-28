abca4482d3c871435b91e6802ab9e4bb74b0dadc
juchouzende <juchouzende@gmail.com>
2025-08-28 15:30:06 -0300

    Commit da Ju

---
+++ calculadora.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Calculadora Simples</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+
+    <h1> Calculadora Simples </h1>
+    <form action='<?php $_SERVER["PHP_SELF"] ?>' method="post">
+        <label for="num1">Num1: </label>
+        <input type="text" name="num1">
+        <label for="num2">Num2: </label>
+        <input type="text" name="num2">
+
+        <input type="submit"> <input type="reset" value="reset">
+    </form>
+
+    <?php
+
+        //Variáveis
+        $num1 = 0;
+        $num2 = 0;
+        $total = 0;
+
+        if(isset($_POST["num1"]) && $_POST["num1"]){
+            //Entradas
+            $num1 = $_POST["num1"];
+            $num2 = $_POST["num2"];
+
+            //Processamento
+            $total = $num1 + $num2;
+
+            //Saída
+            echo "<br> <br> <br>";
+            echo "<h2>" .$num1. " + " .$num2. " = " .$total. "</h2>";
+        }
+
+    ?>
+    
+</body>
+</html>
